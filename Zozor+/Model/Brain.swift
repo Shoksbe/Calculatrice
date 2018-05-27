1848fd1c787c641c32b22c3e0ec9ede980fc1115
Shoksbe <deknyfgregory@gmail.com>
2018-05-27 18:16:13 +0200

    feat(): add Brain class

---
+++ Zozor+/Model/Brain.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  CountOnMe
+//
+//  Created by De knyf Gregory on 27/05/18.
+//  Copyright © 2018 Ambroise Collon. All rights reserved.
+//
+
+import Foundation
+
+class Brain {
+    
+}
