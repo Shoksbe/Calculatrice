bb4e18279c176a0eca287faf08a549f17229e060
Shoksbe <deknyfgregory@gmail.com>
2018-05-28 16:18:30 +0200

    feat(): adding Brain class

--- Zozor+/Model/Brain.swift
+++ Zozor+/Model/Brain.swift
@@ -9,5 +9,71 @@
 import Foundation
 
 class Brain {
+
     
+    var stringNumbers: [String] = [String()]
+    var operators: [String] = ["+"]
+    var index = 0
+
+    var isExpressionCorrect: Bool {
+        if let stringNumber = stringNumbers.last {
+            if stringNumber.isEmpty {
+                return false
+            }
+        }
+        return true
+    }
+
+    var canAddOperator: Bool {
+        if let stringNumber = stringNumbers.last {
+            if stringNumber.isEmpty {
+                return false
+            }
+        }
+        return true
+    }
+
+    
+    func addNewNumber(_ newNumber: Int) {
+        if let stringNumber = stringNumbers.last {
+            var stringNumberMutable = stringNumber
+            stringNumberMutable += "\(newNumber)"
+            stringNumbers[stringNumbers.count-1] = stringNumberMutable
+        }
+    }
+
+    func calculateTotal()-> Int {
+        var total = 0
+        for (i, stringNumber) in stringNumbers.enumerated() {
+            if let number = Int(stringNumber) {
+                if operators[i] == "+" {
+                    total += number
+                } else if operators[i] == "-" {
+                    total -= number
+                }
+            }
+        }
+
+        return total
+    }
+
+    func clear() {
+        stringNumbers = [String()]
+        operators = ["+"]
+        index = 0
+    }
+
+    func updateDisplay()-> String {
+        var text = ""
+        for (i, stringNumber) in stringNumbers.enumerated() {
+            // Add operator
+            if i > 0 {
+                text += operators[i]
+            }
+            // Add number
+            text += stringNumber
+        }
+        return text
+    }
+
 }
