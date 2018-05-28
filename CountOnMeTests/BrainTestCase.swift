bf734dd17658900397db04ed8a58321c839d9529
Shoksbe <deknyfgregory@gmail.com>
2018-05-28 17:06:35 +0200

    feat(): add test files

---
+++ CountOnMeTests/BrainTestCase.swift
@@ -0,0 +1,15 @@
+//
+//  BrainTestCase.swift
+//  CountOnMeTests
+//
+//  Created by Gregory De knyf on 28/05/18.
+//  Copyright © 2018 Ambroise Collon. All rights reserved.
+//
+
+import XCTest
+@testable import CountOnMe
+
+class BrainTestCase: XCTestCase {
+    
+}
+
